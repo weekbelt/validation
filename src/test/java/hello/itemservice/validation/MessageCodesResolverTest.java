f86eda7428a01ca341107e4b9f354da942cf990c
weekbelt <vfrvfr4207@gmail.com>
2021-07-04 00:44:11 +0900

    Validation

---
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,32 @@
+package hello.itemservice.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    public void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    public void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+
+        assertThat(messageCodes).containsExactly(
+            "required.item.itemName",
+            "required.itemName",
+            "required.java.lang.String",
+            "required"
+        );
+    }
+
+}
